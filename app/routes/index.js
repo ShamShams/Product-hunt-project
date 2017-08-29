c1824ca1b72780e8a6d394b651396a553573af10
ShamShams <shamila.remtoula@hotmail.fr>
2017-08-29 12:47:06 +0200

    Add mongoose

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,3 +4,5 @@ const router = express.Router();
 router.get('/', (request, response) => {
   response.render('list');
 });
+
+module.exports = router;
--- app/server.js
+++ app/server.js
@@ -2,13 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./routes/index');
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/products');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('view-engine', 'pug');
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(router);
 
