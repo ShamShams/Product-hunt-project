aeab681ed587eaa6db661cc22c51cef7c6a414b9
ShamShams <shamila.remtoula@hotmail.fr>
2017-11-02 20:00:14 +0100

    Add upload images

--- app/models/product.js
+++ app/models/product.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const product = mongoose.Schema({
-  name: String,
-  tagline: String,
-  url: String,
+  name: { type: String },
+  tagline: { type: String },
+  url: { type: String },
   createdAt: { type: Date, default: Date.now },
+  photo: { type: String },
 });
 
 module.exports = mongoose.model('Product', product);
--- app/routes/products.js
+++ app/routes/products.js
@@ -2,27 +2,27 @@ const express = require('express');
 const path = require('path');
 const multer = require('multer');
 
-const router = express.Router();
 const Product = require('../models/product');
 
-// const storage = multer.diskStorage({
-//   destination: (request, file, callback) => {
-//     callback(null, path.resolve(__dirname, 'public', 'upload')),
-//   },
-//   filename: (request, file, callback) => {
-//     callback(null, `${request.body.name}.${file.originalname.split('.')[1]}`); // Va séparer le nom de l'extension et on ne garde que l'extension.
-//   },
-// });
-//
-// const upload = multer({
-//   storage
-// });
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: (request, file, callback) => {
+    callback(null, path.resolve('app', 'public', 'uploads'));
+  },
+  filename: (request, file, callback) => {
+    callback(null, `${request.body.name.toLowerCase()}.${file.originalname.split('.')[1]}`); // nom du fichier image. split va séparer le nom de l'extension et on ne garde que l'extension ([1])
+  },
+});
+
+const upload = multer({ storage });
 
 // Afficher la liste des produits
 router.get('/products', (request, response) => {
   Product.find({}, (error, products) => {
     if (error) response.send(error);
     response.render('products', { products });
+    // response.json(products);
   });
 });
 
@@ -31,6 +31,7 @@ router.get('/products/:id', (request, response) => {
   Product.findById(request.params.id, (error, product) => {
     if (error) response.send(error);
     response.render('product', { product });
+    // response.json(product);
   });
 });
 
@@ -39,19 +40,21 @@ router.get('/add', (request, response) => {
   response.render('add_product');
 });
 // -> Enregistrer le nouveau produit et rediriger vers la liste des produits
-router.post('/add', (request, response) => {
-  // upload.single('photo') -> après '/add',
+router.post('/add', upload.single('photo'), (request, response) => {
   const product = new Product(request.body);
-  // product.photo
+  product.photo = `/${request.file.destination.split('/').pop()}/${request.file.filename}`; // le nom que le fichier aura dans la db pour pouvoir la display.
   product.save((error) => {
     if (error) response.send(error);
-    response.redirect('/products');
+    response.redirect('/products'); // TODO: Faire apparaitre la page du produit au lieu de la liste.
+    // response.json(product);
   });
+  console.log(product.photo);
 });
 
-// Editer un produit -> Enregistrer les modifications et rediriger vers la page produit
-router.post('/edit/:id', (request, response) => {
+// Editer un produit* -> Enregistrer les modifications et rediriger vers la page produit
+router.post('/edit/:id', upload.single('photo'), (request, response) => {
   Product.findByIdAndUpdate(request.params.id, request.body, (error) => {
+    // request.body.photo = `/${request.file.destination.split('/').pop()}/${request.file.filename}`;
     if (error) response.send(error);
     response.redirect(`/products/${request.params.id}`);
   });
@@ -73,3 +76,5 @@ router.get('/remove/:id', (request, response) => {
 });
 
 module.exports = router;
+
+// *À cause de la mauvaise implémentation des méthodes HTTP (pour Ajax) par certains navigateurs (et la norme HTML qui ne supporte que les méthodes GET et POST pour les formulaires), cette méthode est souvent utilisée en remplacement de la requête PUT, qui devrait être utilisée pour la mise à jour de ressources.
